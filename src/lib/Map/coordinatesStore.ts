de03630364cacc0cc3674e82cce46bd5b495e53d
Augustin Chassine <augustin.chassine@mabsilico.com>
2021-12-19 16:09:27 +0100

    refactor: coordinates Store - types and refactor

--- src/lib/Map/coordinatesStore.ts
+++ src/lib/Map/coordinatesStore.ts
@@ -1,6 +1,7 @@
 // Here the reactive svelte stores handling the maps coordinates and the
 import { derived, writable } from 'svelte/store';
-import type { Writable } from 'svelte/store';
+import type { Readable, Writable } from 'svelte/store';
+import type { SlackmapCluster, SlackmapSpot } from '$lib/slackMapApiTypes';
 type LeafletBounds = {
   _southWest: LeafletCoords;
   _northEast: LeafletCoords;
@@ -11,53 +12,57 @@ type LeafletCoords = {
   lng: number;
 };
 
+type ViewPortCoordinates = {
+  centerPos: LeafletCoords;
+  bounds: LeafletBounds;
+  zoom: number;
+};
+
 const API_URLS = {
   clusters: 'api/clusters.json',
   spots: 'api/spots.json',
   items: 'api/items.json'
 };
 
-// reactively expose the map center point
-export const centerPos: Writable<LeafletCoords> | Writable<null> = writable(null);
+// Reactively expose the map component coordinates features (zoom, centerPosition, mapBounds)
+export const viewPortCoordinates: Writable<ViewPortCoordinates | Record<string, never>> = writable(
+  {}
+);
 
-// reactively expose the map boundaries
-export const bounds: Writable<LeafletBounds> | Writable<null> = writable(null);
-
-export const zoom = writable(0);
-
-export const viewPortCoordinates = writable({});
-
-// This is a reactive derived asynchronous store. It is derived from the `bounds` store
+// This is a reactive derived asynchronous store. It is derived from the `viewPortCoordinates` store
 // whenever the map bounds change:
 // - we query the server, which in turn queries the slackmap API with the right parameters
 // - the result is then reactively exposed to consumer components
-export const dataFromAPI = derived(viewPortCoordinates, ($viewPortCoordinates, set) => {
-  if ($viewPortCoordinates.bounds && $viewPortCoordinates.bounds['_southWest']) {
-    const SWlat = $viewPortCoordinates.bounds['_southWest'].lat;
-    const SWlng = $viewPortCoordinates.bounds['_southWest'].lng;
-    const NElat = $viewPortCoordinates.bounds['_northEast'].lat;
-    const NElng = $viewPortCoordinates.bounds['_northEast'].lng;
+export const dataFromAPI: Readable<Array<SlackmapCluster> | Array<SlackmapSpot>> = derived(
+  viewPortCoordinates,
+  ($viewPortCoordinates, set) => {
+    if ($viewPortCoordinates?.bounds && $viewPortCoordinates.bounds['_southWest']) {
+      const SWlat = $viewPortCoordinates.bounds['_southWest'].lat;
+      const SWlng = $viewPortCoordinates.bounds['_southWest'].lng;
+      const NElat = $viewPortCoordinates.bounds['_northEast'].lat;
+      const NElng = $viewPortCoordinates.bounds['_northEast'].lng;
 
-    const zoom = $viewPortCoordinates.zoom;
-    const center = $viewPortCoordinates.centerPos;
+      const zoom = $viewPortCoordinates.zoom;
+      const center = $viewPortCoordinates.centerPos;
 
-    let APIFeaturesEndpoint = '';
+      let APIFeaturesEndpoint = '';
 
-    if (zoom >= 17) {
-      APIFeaturesEndpoint = API_URLS.spots;
+      if (zoom >= 17) {
+        APIFeaturesEndpoint = API_URLS.spots;
+      } else {
+        APIFeaturesEndpoint = API_URLS.clusters;
+      }
+
+      // here we fetch the server endpoint (which will in turn query the slackmap API)
+      fetch(
+        `${APIFeaturesEndpoint}?bbox=${SWlng},${SWlat},${NElng},${NElat}&zoom=${zoom}&center[type]=Feature&center[geometry][type]=Point&center[geometry][coordinates][]=${center.lng},${center.lat}`
+      )
+        .then((r) => r.json())
+        .then((d) => {
+          set(d);
+        });
     } else {
-      APIFeaturesEndpoint = API_URLS.clusters;
+      set([]);
     }
-
-    // here we fetch the server endpoint (which will in turn query the slackmap API)
-    fetch(
-      `${APIFeaturesEndpoint}?bbox=${SWlng},${SWlat},${NElng},${NElat}&zoom=${zoom}&center[type]=Feature&center[geometry][type]=Point&center[geometry][coordinates][]=${center.lng},${center.lat}`
-    )
-      .then((r) => r.json())
-      .then((d) => {
-        set(d);
-      });
-  } else {
-    set([]);
   }
-});
+);
